4f0b18f35555e45f1160742936f181adb678400e
Sethyshola20 <sethylunelo@gmail.com>
2022-08-30 18:08:18 +0200

    new styles

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
-import './assets/App.scss';
-import Representative from './components/Representatives';
-import React, {useState, useEffect} from 'react';
+import "./assets/App.scss";
+import Representative from "./components/Representatives";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [representatives, setRepresentatives] = useState([]);
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(true);
 
-  const [representatives, setRepresentatives] = useState([])
-  const [search, setSearch] = useState('')
-  const [loading, setLoading] = useState(true)
-  
+  const fetchRepresentative = async () => {
+    const res = await fetch("https://www.nosdeputes.fr/deputes/json");
+    const data = await res.json();
+    console.log(data);
 
-  const fetchRepresentative = async() => {
-    const res = await fetch('https://www.nosdeputes.fr/deputes/json')
-    const data = await res.json()
-    console.log(data)
-    
-    if(data.deputes) {
-      setRepresentatives(data.deputes)
-      setLoading(false)
+    if (data.deputes) {
+      setRepresentatives(data.deputes);
+      setLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchRepresentative()
-  }, [])
+    fetchRepresentative();
+  }, []);
   return (
     <>
-      <form search={search} setSearch={setSearch} >
-          <input className="search_input" type='text' placeholder='Nom, parti, departement...' onChange={(event)=> setSearch(event.target.value.toLowerCase())}/>
-      </form>
-        {loading ? <div>loading...</div> : <h1>Les députés de la 14e législature:</h1>}
+      <div className="form">
+        <form search={search} setSearch={setSearch}>
+          <input
+            className="search_input"
+            type="text"
+            placeholder="Nom, parti, departement..."
+            onChange={(event) => setSearch(event.target.value.toLowerCase())}
+          />
+        </form>
+      </div>
+      {loading ? (
+        <div>loading...</div>
+      ) : (
+        <h1>Les députés de la 14e législature:</h1>
+      )}
       <Representative representatives={representatives} search={search} />
     </>
   );
